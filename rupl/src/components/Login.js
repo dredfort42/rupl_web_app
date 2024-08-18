049f420c838e3d27914d85c3d8fbb8ae45b9542c
Dmitry Novikov <dredfort.42@gmail.com>
2024-08-18 21:42:32 +0300

    login

---
+++ rupl/src/components/Login.js
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+        <div>
+            <h2>Login form</h2>
+            <p>Username</p>
+            <p>Password</p>
+        </div>
+    );
+}
+
+export default Login;
