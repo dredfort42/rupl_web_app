92faf465298a88768997b98fc498021f8207f1ae
Dmitry Novikov <dredfort.42@gmail.com>
2024-09-11 20:31:03 +0100

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-    return <div>Home</div>;
+    return (
+        <div>
+            <div>
+                <h1>rupl</h1>
+                <p>Your personal running coach</p>
+                <p>
+                    Track your runs, get insights and improve your performance
+                </p>
+            </div>
+            <div>
+                <h1>GitHub</h1>
+                <div>
+                    <Link to="https://github.com/dredfort42/rupl">
+                        Open source project for everyone who wants to run
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
